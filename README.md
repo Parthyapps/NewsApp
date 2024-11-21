d1ad8fb3d5cef2c7a6fa8c86320e5c5fd5288b6b
parthiban <parthiban.subramaniyan@v-nova.com>
2024-11-21 23:26:12 +0530

    Added Dependency Injection (Dagger) library and updated README file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-News App Using Kotlin
+## News App Using Kotlin
+
+## Project Structure
+### Packages:
+ - di (Dependency Injection)
+ - network (Retrofit services)
+ - repository (Data handling)
+ - ui (Activities, Fragments, ViewModels)
+ - database (Room Entities and DAOs)
+ - utils (Helper classes)
+
+## Dependency Injection (Dagger)
+   - Add dependencies to build.gradle:
+```groovy
+    implementation 'com.google.dagger:dagger:2.x'
+    kapt 'com.google.dagger:dagger-compiler:2.x'
+```
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kapt)
 }
 
 android {
     namespace = "com.parthyapps.newsapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.parthyapps.newsapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -42,6 +43,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // dagger library
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
