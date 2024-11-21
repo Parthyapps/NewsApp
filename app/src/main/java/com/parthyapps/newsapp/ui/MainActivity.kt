32e48383cbd4fccc215a479b3eb47a392de4fdd5
parthiban <parthiban.subramaniyan@v-nova.com>
2024-11-21 22:33:38 +0530

    MVVM architecture pattern Model, View, and ViewModel

--- app/src/main/java/com/parthyapps/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/parthyapps/newsapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.parthyapps.newsapp
+package com.parthyapps.newsapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.parthyapps.newsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
